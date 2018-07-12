06afce2c1d1c8994f760282954c0587cdf794350
Marco Espinoza <marco.espinoza@seidor.com>
2018-07-12 14:16:26 -0500

    Agregar 3ra subnet

--- main.tf
+++ main.tf
@@ -21,9 +21,9 @@ module "vpc" {
   cidr = "10.1.0.0/16"
 
   #ZONAS DE LA VPC Y REDES PRIVADAS Y PUBLICAS
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
-  public_subnets  = ["10.1.3.0/24", "10.1.4.0/24"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.4.0/24", "10.1.5.0/24"]
 
   tags = {
     Owner       = "user"
